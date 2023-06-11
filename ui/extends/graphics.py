f80e54498999fc865bed782a78a6af3e92487a57
283375 <bilibili283375@163.com>
2023-06-11 23:52:20 +0800

    impr: GraphicsViewZoomWrapper supports zoom by scrolling

--- ui/extends/graphics.py
+++ ui/extends/graphics.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QGraphicsView
-from PySide6.QtCore import QPointF, QObject
+from PySide6.QtCore import QEvent, QObject, QPointF, Qt
+from PySide6.QtGui import QMouseEvent, QWheelEvent
+from PySide6.QtWidgets import QApplication, QGraphicsView
 
 
 class GraphicsViewZoomWrapper(QObject):
@@ -9,12 +10,19 @@ class GraphicsViewZoomWrapper(QObject):
     """
 
     def __init__(self, view: QGraphicsView):
-        self.__view = view
+        super().__init__(view)
         self.target_scene_pos = QPointF()
         self.target_viewport_pos = QPointF()
 
-    def setView(self, view: QGraphicsView):
+        self.__modifier = Qt.KeyboardModifier.ControlModifier
+        self.__zoom_factor_base = 1.0015
+
+        view.viewport().installEventFilter(self)
+        view.setMouseTracking(True)
         self.__view = view
+
+    def setModifier(self, modifier: Qt.KeyboardModifier):
+        self.__modifier = modifier
 
     def zoom(self, factor: float):
         target_scene_pos = QPointF(
@@ -41,3 +49,27 @@ class GraphicsViewZoomWrapper(QObject):
             QPointF(self.__view.mapFromScene(target_scene_pos)) - delta_viewport_pos
         )
         self.__view.centerOn(self.__view.mapToScene(viewport_center.toPoint()))
+
+    def zoomIn(self):
+        self.zoom(1.05)
+
+    def zoomOut(self):
+        self.zoom(0.95)
+
+    def eventFilter(self, object: QObject, event: QEvent):
+        if isinstance(event, QMouseEvent):
+            delta = QPointF(self.target_viewport_pos - event.pos())
+            if abs(delta.x()) > 5 or abs(delta.y()) > 5:
+                self.target_viewport_pos = event.pos()
+                self.target_scene_pos = self.__view.mapToScene(event.pos())
+        elif isinstance(event, QWheelEvent):
+            if (
+                QApplication.keyboardModifiers() == self.__modifier
+                and abs(event.angleDelta().y()) > 0
+            ):
+                angle = event.angleDelta().y()
+                factor = pow(self.__zoom_factor_base, angle)
+                self.zoom(factor)
+                return True
+
+        return False
