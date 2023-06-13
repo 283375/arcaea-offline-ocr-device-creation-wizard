84e64ef1915664d826d3a8bada4e95860625c5ee
283375 <bilibili283375@163.com>
2023-06-14 03:08:15 +0800

    impr: FileSelector

--- ui/implements/components/fileSelector.py
+++ ui/implements/components/fileSelector.py
@@ -1,9 +1,11 @@
 from ui.designer.components.fileSelector_ui import Ui_FileSelector
 from PySide6.QtWidgets import QWidget, QFileDialog
-from PySide6.QtCore import Property, Slot, Qt
+from PySide6.QtCore import Signal, Slot, QFile, Qt
 
 
 class FileSelector(Ui_FileSelector, QWidget):
+    accepted = Signal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.fileDialog = QFileDialog(self)
@@ -11,10 +13,22 @@ class FileSelector(Ui_FileSelector, QWidget):
         self.setupUi(self)
         self.elidedLabel.setElideMode(Qt.TextElideMode.ElideMiddle)
 
+        self.fileDialog.accepted.connect(self.accepted)
+        self.selectedFiles = self.fileDialog.selectedFiles
+        self.setNameFilters = self.fileDialog.setNameFilters
+
     @Slot()
     def on_pushButton_clicked(self):
         self.fileDialog.open()
 
     @Slot()
-    def on_fileDialog_accepted(self):
-        self.elidedLabel.setText("<br>".join(self.fileDialog.selectedFiles()))
+    def on_fileDialog_filesSelected(self):
+        self.__updateLabel(self.fileDialog.selectedFiles())
+
+    def __updateLabel(self, list_str: list[str]):
+        self.elidedLabel.setText("<br>".join(list_str))
+
+    def selectFile(self, filename: str):
+        if QFile(filename).exists():
+            self.fileDialog.selectFile(filename)
+            self.__updateLabel([filename])
